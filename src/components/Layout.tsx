d73575d5ebff1da86f8eda9f91e47954a6b3eb5c
Michael McNees <mcnees.michael@gmail.com>
2022-10-25 18:52:42 -0400

    "Fixed" Kenway's (awful) preferred main content positioning

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: { children: ReactElement }) => {
       <Navbar />
       <main className='flex-1 bg-gray-100'>
         <div className='py-6'>
-          <div className='mx-auto max-w-7xl px-4 sm:px-6 md:px-8'>{children}</div>
+          <div className='max-w-7xl px-4 sm:px-6 md:px-8'>{children}</div>
         </div>
       </main>
     </div>
