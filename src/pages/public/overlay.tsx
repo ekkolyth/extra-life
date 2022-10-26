bbe5450f5a98fb598e3930f385f4f6102019028b
Michael McNees <mcnees.michael@gmail.com>
2022-10-25 23:05:28 -0400

    Added Scolei to donation bar

--- src/pages/public/overlay.tsx
+++ src/pages/public/overlay.tsx
@@ -3,15 +3,19 @@
 import { useQuery } from 'react-query'
 import { fetchStats, formatter, percentage } from '../../utils/donorDrive'
 import LurkMerch from '../../assets/img/lurk-merch.png'
+import Scolei from '../../assets/img/scolei.png'
 import TimeLeft from '../../components/overlay/TimeLeft'
 import Confetti from 'react-confetti'
 import { useEffect, useState } from 'react'
 import { Transition } from '@headlessui/react'
+import { classNames } from '../../utils/style'
 
 const Overlay = () => {
   const [confetti, setConfetti] = useState(false)
   const [timesUp, setTimesUp] = useState(false)
   const { data, error, isLoading } = useQuery(['extralife', 'donors'], () => fetchStats('478888'))
+
+  const donationPercentage = percentage(data?.sumDonations, data?.fundraisingGoal)
 
   // Once times up turns to true, set confetti to true
   // After 1 minute, set confetti to false
@@ -43,15 +47,17 @@ const Overlay = () => {
       <div className='absolute bottom-12 left-0 right-0 w-full flex justify-center'>
         <div
           style={{ width: 934 }}
-          className='bg-gray-600 border-8 border-black text-white rounded-full text-3xl font-bold text-center relative overflow-hidden'>
-          <div
-            className='h-16 bg-purple-bar-1 rounded-full'
-            style={{ width: `${percentage(data?.sumDonations, data?.fundraisingGoal)}%` }}></div>
+          className='bg-gray-600 border-8 border-black text-white rounded-full text-3xl font-bold text-center relative'>
+          <div className='h-16 bg-purple-bar-1 rounded-full' style={{ width: `${donationPercentage}%` }}></div>
           <div className='absolute inset-0 flex items-center justify-center'>
             <p>{formatter.format(data?.sumDonations)}</p>
           </div>
-          <div className='absolute bottom-0 right-64 w-16 h-16'>
-            <img src={LurkMerch.src} />
+          <div
+            className={classNames(
+              donationPercentage >= 100 ? 'w-32 h-32 right-48' : 'w-16 h-16 right-60',
+              'absolute bottom-0'
+            )}>
+            <img src={donationPercentage >= 100 ? Scolei.src : LurkMerch.src} />
           </div>
         </div>
       </div>
