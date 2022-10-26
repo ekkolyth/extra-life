dd608bd1fdc97fdfd271d0ba16d4be478b623a41
Michael McNees <mcnees.michael@gmail.com>
2022-10-25 22:55:30 -0400

    Completed time left functionality

--- src/components/overlay/TimeLeft.tsx
+++ src/components/overlay/TimeLeft.tsx
@@ -1,8 +1,10 @@
-// Use dayjs to display the current countdown to November 13 at 10am in hours, minutes, and seconds
+// Display the current countdown to November 13 at 10am in hours, minutes, and seconds
+// Bonus fun when we reach zero
 
 import { useEffect, useState } from 'react'
 
-const TimeLeft = () => {
+const TimeLeft = ({ timesUp }: { timesUp: (value: boolean) => void }) => {
+  const [displayTime, setDisplayTime] = useState(true)
   const [timeLeft, setTimeLeft] = useState('00h 00m 00s')
 
   useEffect(() => {
@@ -21,6 +23,14 @@ const TimeLeft = () => {
       const mDisplay = m % 60
       const sDisplay = s % 60
 
+      if (hDisplay <= 0 && mDisplay <= 0 && sDisplay <= 0) {
+        setDisplayTime(false)
+        timesUp(true)
+      } else {
+        setDisplayTime(true)
+        timesUp(false)
+      }
+
       setTimeLeft(`${hDisplay}h ${mDisplay}m ${sDisplay}s`)
     }, 1000)
 
@@ -29,8 +39,18 @@ const TimeLeft = () => {
 
   return (
     <div className='bg-purple-bar-1 w-72 rounded-xl py-4 px-6 shadow-super'>
-      <p className='font-bold text-3xl text-white mb-2'>time left:</p>
-      <p className='font-bold text-4xl text-white truncate'>{timeLeft}</p>
+      {displayTime ? (
+        <>
+          <p className='font-bold text-3xl text-white mb-2'>time left:</p>
+          <p className='font-bold text-4xl text-white truncate'>{timeLeft}</p>
+        </>
+      ) : (
+        <p className='font-bold text-4xl text-white'>
+          Fuck
+          <br />
+          Penn State!
+        </p>
+      )}
     </div>
   )
 }
--- src/pages/public/overlay.tsx
+++ src/pages/public/overlay.tsx
@@ -4,12 +4,42 @@ import { useQuery } from 'react-query'
 import { fetchStats, formatter, percentage } from '../../utils/donorDrive'
 import LurkMerch from '../../assets/img/lurk-merch.png'
 import TimeLeft from '../../components/overlay/TimeLeft'
+import Confetti from 'react-confetti'
+import { useEffect, useState } from 'react'
+import { Transition } from '@headlessui/react'
 
 const Overlay = () => {
+  const [confetti, setConfetti] = useState(false)
+  const [timesUp, setTimesUp] = useState(false)
   const { data, error, isLoading } = useQuery(['extralife', 'donors'], () => fetchStats('478888'))
+
+  // Once times up turns to true, set confetti to true
+  // After 1 minute, set confetti to false
+  useEffect(() => {
+    if (timesUp) {
+      setConfetti(true)
+      setTimeout(() => {
+        setConfetti(false)
+      }, 30000)
+    }
+  }, [timesUp])
 
   return (
     <div style={{ width: 1920, height: 1080 }} className='relative bg-green-500'>
+      <Transition
+        show={confetti}
+        enter='transition-opacity duration-75'
+        enterFrom='opacity-0'
+        enterTo='opacity-100'
+        leave='transition-opacity duration-1000'
+        leaveFrom='opacity-100'
+        leaveTo='opacity-0'>
+        <Confetti
+          width={1920}
+          height={1080}
+          colors={['#5D41DE', '#0E0E10', '#1D4C6C', '#47C2E2', '#6B4BFF', '#5D41DE']}
+        />
+      </Transition>
       <div className='absolute bottom-12 left-0 right-0 w-full flex justify-center'>
         <div
           style={{ width: 934 }}
@@ -26,7 +56,7 @@ const Overlay = () => {
         </div>
       </div>
       <div className='absolute bottom-14 right-12'>
-        <TimeLeft />
+        <TimeLeft timesUp={value => setTimesUp(value)} />
       </div>
     </div>
   )
