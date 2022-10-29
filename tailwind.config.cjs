49c9417218715899b2df493c6c6f1ac1910a2671
Michael McNees <mcnees.michael@gmail.com>
2022-10-29 15:27:17 -0400

    Added fullscreen donations page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        display: ['Heavitas Regular', ...defaultTheme.fontFamily.sans]
+      },
+      fontSize: {
+        '10xl': '16rem'
+      },
       colors: {
         'primary': '#5D41DE',
         'black': '#0E0E10',
@@ -10,6 +18,10 @@ module.exports = {
         'el-light-blue': '#47C2E2',
         'purple-bar-1': '#6B4BFF',
         'purple-bar-2': '#5D41DE'
+      },
+      backgroundImage: {
+        'grid-pattern': "url('/assets/img/grid-pattern.png')",
+        'purple-overlay': "url('/assets/img/purple-overlay.png')"
       }
     }
   },
