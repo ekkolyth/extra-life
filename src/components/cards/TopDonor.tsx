efcf3672f712b4c57fc7b7764ba54f910423f67b
Michael McNees <mcnees.michael@gmail.com>
2022-11-12 12:08:27 -0500

    Fixed build error on top donor card

--- src/components/cards/TopDonor.tsx
+++ src/components/cards/TopDonor.tsx
@@ -28,14 +28,14 @@ const TopDonor = () => {
     <Card title='Top Donor' icon={<BanknotesIcon />}>
       <div className='my-4 flex items-center'>
         <div className='mr-2 flex items-center'>
-          <Image height={64} width={64} alt='Top Donor' src={data.avatarImageURL} />
+          {data?.avatarImageURL && <Image height={64} width={64} alt='Top Donor' src={data.avatarImageURL} />}
         </div>
         <div className='w-full flex justify-between items-center'>
           <div>
             <p className='font-semibold text-gray-800'>{data?.displayName}</p>
             <p className='text-xs text-gray-500'>{data?.numDonations} donations</p>
           </div>
-          <p className='font-semibold text-gray-800'>{formatter.format(data?.sumDonations)}</p>
+          <p className='font-semibold text-gray-800'>{formatter.format(data?.sumDonations ?? 0)}</p>
         </div>
       </div>
     </Card>
