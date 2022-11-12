ed6f82646bf03b6ecb83f05d454088430ac116ff
Michael McNees <mcnees.michael@gmail.com>
2022-11-12 01:40:41 -0500

    added donor type definition

--- src/utils/donorDrive.ts
+++ src/utils/donorDrive.ts
@@ -1,5 +1,14 @@
 import axios from 'axios'
 
+export type Donor = {
+  displayName: string
+  donorID: string
+  avatarImageURL: string
+  modifiedDateUTC: string
+  sumDonations: number
+  numDonations: number
+}
+
 export type Donation = {
   displayName: string
   donorID: string
@@ -30,11 +39,11 @@ export const formatter = new Intl.NumberFormat('en-US', {
 export const percentage = (donations: number, goal: number) => Math.floor((donations / goal) * 100)
 
 export const fetchTopDonor = async (id: string) => {
-  return await axios
+  return (await axios
     .get(
       `https://extra-life.org/api/participants/${id}/donors?limit=1&orderBy=sumDonations%20DESC&where=amountVisibility%20%3D%20ALL%20AND%20sumDonations%20%3E%200`
     )
-    .then(res => res.data[0])
+    .then(res => res.data[0])) as Donor
 }
 
 export const fetchTopDonation = async (id: string) => {
