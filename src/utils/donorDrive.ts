196b464dc107524a813e7a0eb981ceb31ec3a968
Michael McNees <mcnees.michael@gmail.com>
2022-11-12 01:32:44 -0500

    fixed build error

--- src/utils/donorDrive.ts
+++ src/utils/donorDrive.ts
@@ -30,11 +30,11 @@ export const formatter = new Intl.NumberFormat('en-US', {
 export const percentage = (donations: number, goal: number) => Math.floor((donations / goal) * 100)
 
 export const fetchTopDonor = async (id: string) => {
-  return (await axios
+  return await axios
     .get(
       `https://extra-life.org/api/participants/${id}/donors?limit=1&orderBy=sumDonations%20DESC&where=amountVisibility%20%3D%20ALL%20AND%20sumDonations%20%3E%200`
     )
-    .then(res => res.data[0])) as Donation
+    .then(res => res.data[0])
 }
 
 export const fetchTopDonation = async (id: string) => {
