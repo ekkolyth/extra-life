428d6bf9c5eea3a4c3db424c7c08cd2700d63603
Michael McNees <mcnees.michael@gmail.com>
2023-10-10 16:59:37 -0400

    Made controller icon named export

--- src/components/icons/Controller.tsx
+++ src/components/icons/Controller.tsx
@@ -1,4 +1,4 @@
-const ControllerIcon = ({ ...props }) => {
+export const ControllerIcon = ({ ...props }) => {
   return (
     <svg {...props} viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
@@ -32,5 +32,3 @@ const ControllerIcon = ({ ...props }) => {
     </svg>
   )
 }
-
-export default ControllerIcon
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { CalendarIcon } from '@heroicons/react/24/outline'
-import ControllerIcon from '@/components/icons/controller'
+import { ControllerIcon } from '@/components/icons/controller'
 import { usePathname } from 'next/navigation'
 import { Button } from './ui/button'
 import Link from 'next/link'
