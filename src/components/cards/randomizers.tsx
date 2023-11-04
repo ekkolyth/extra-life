04005802393ac13486c6d84e70f7e807df56ce5a
Michael McNees <mcnees.michael@gmail.com>
2023-11-04 13:13:41 -0400

    Added wheel spins to the admin page

--- src/components/cards/randomizers.tsx
+++ src/components/cards/randomizers.tsx
@@ -9,6 +9,8 @@ import Card from './card'
 import * as Ably from 'ably'
 import { ably } from '@/lib/ably'
 import { Button } from '@/components/ui/button'
+import { useQuery } from 'react-query'
+import { fetchWheelSpinDonations } from '@/utils/donor-drive'
 
 interface RandomizerCardProps {
   randomizers: Randomizer[]
@@ -16,7 +18,32 @@ interface RandomizerCardProps {
 
 export function RandomizerCard(props: RandomizerCardProps) {
   const { randomizers } = props
+  const [left, setLeft] = useState(0)
+  const [total, setTotal] = useState(0)
   const [channel, setChannel] = useState<Ably.Types.RealtimeChannelPromise | null>(ably.channels.get('randomizers'))
+
+  useQuery(
+    ['extralife', 'wheelSpinDonations'],
+    () => fetchWheelSpinDonations(String(process.env.NEXT_PUBLIC_DONORDRIVE_ID)),
+    {
+      refetchInterval: 5000,
+      onSuccess(data) {
+        setTotal(data.length)
+      }
+    }
+  )
+  useQuery(
+    ['redemptions'],
+    () => fetch(`/api/randomizers/cloglmz700000lc08agvmotp1/redemptions`).then(res => res.json()),
+    {
+      cacheTime: 0,
+      refetchInterval: 5000,
+      onSuccess(data) {
+        console.log('redemptions', data)
+        setLeft(total - data.length)
+      }
+    }
+  )
 
   useEffect(() => {
     if (channel) {
@@ -49,6 +76,12 @@ export function RandomizerCard(props: RandomizerCardProps) {
       <p className='text-destructive font-bold text-xs -mt-6 mb-4'>
         Do not close this page once a randomizer is triggered, until it is finished spinning.
       </p>
+      <div>
+        <h3>Spins</h3>
+        <p className='font-bold text-4xl text-white'>
+          {left} / {total}
+        </p>
+      </div>
       <ul>
         {randomizers.length > 0 ? (
           randomizers.map(randomizer => (
