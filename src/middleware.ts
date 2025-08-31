db074045a6365c4ff61712355c7c6e0e781b6d8f
Mike Kenway <mike@mikekenway.com>
2025-08-31 17:09:52 -0400

    added shadcn and clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,12 @@
-export { default } from 'next-auth/middleware'
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!donations|overlay|api|assets|_next/static|_next/image|favicon.ico).*)'
-  ]
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
